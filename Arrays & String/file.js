ff8f70685db2d2a885eb9b27e04b31efaac02235
Nischal <gupta.nischal014@gmail.com>
2022-12-25 13:20:53 +0530

    started adding problems

--- Arrays & String/file.js
+++ Arrays & String/file.js
@@ -1,21 +1,34 @@
 // Find a peak element which is not smaller than its neighbors	
 
-let arr = [1,2,3,4,5,6,7,8,9,10,9] 
+let arr = [1,2,3,4,5,6,7,8,9,10,97] 
+console.log('----__---->',arr.length)
 
-console.log(arr.at(-2)) //--> peak is @ 10
+for(let i=0; i<= arr.length;i++){
+
+    console.log('-->',arr[arr.length-1])
+}
+//==========================================================
 
 // Find the minimum and maximum element in an array
+//==========================================================
 
 // Write a program to reverse the array
+//==========================================================
 
 // Find the Kth largest and Kth smallest number in an array
+//==========================================================
 
 // Find the occurrence of an integer in the array
+//==========================================================
 
 // Sort the array of 0s, 1s, and 2s
+//==========================================================
 
 // Subarray with given Sum
+//==========================================================
 
 // Move all the negative elements to one side of the array
+//==========================================================
 
 // Find the Union and Intersection of the two sorted arrays
+//==========================================================
